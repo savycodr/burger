0a0774732342ff08124754c32697d427d30f4625
savycodr <46404719+savycodr@users.noreply.github.com>
2019-04-24 20:18:41 -0400

    added ability to connect to remote JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,21 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "burgers_db"
-});
+var connection;
+
+// Notice	process.env.JAWSDB_URL lets	us	plug	in	your	connection	details	with	just	one	object	property.	When	you set	up	the	JawsDB	provision,	Heroku	saved	the	connection	info	in	an	environmental	variable (your credentials are well hidden)
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burgers_db"
+  });
+};
 
 // Make connection.
 connection.connect(function(err) {
